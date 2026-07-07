40ec2eaf82fbfc98352d38e79e5ac6f457f0c0bb
mvieira9 <m.augusto.lgs@gmail.com>
2026-07-07 08:34:36 -0300

    adicionado ODS 4

--- js/cicloImagens.js
+++ js/cicloImagens.js
@@ -1,4 +1,5 @@
 const odsInfo = [
+    {text: "ODS 4 - Educação de Qualidade", image:"img/sala de aula.jpg"},
     {text: "ODS 6 - Água Potável e Saneamento", image:"img/agua.jpg"},
     {text: "ODS 7 - Energia Limpa e Acessível", image:"img/energia.jpg"},
     {text: "ODS 11 - Cidades Sustentáveis", image:"img/cidade.jpg"},
