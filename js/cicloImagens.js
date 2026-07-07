3850051798dcc7c949682d2aa5c1ae0037dbc2f2
mvieira9 <m.augusto.lgs@gmail.com>
2026-07-07 10:58:20 -0300

    trocamos as ODS's

--- js/cicloImagens.js
+++ js/cicloImagens.js
@@ -1,13 +1,8 @@
 const odsInfo = [
     {text: "ODS 4 - Educação de Qualidade", image:"img/sala de aula.jpg"},
-    {text: "ODS 6 - Água Potável e Saneamento", image:"img/agua.jpg"},
+    {text: "ODS 6 - Água Limpa e Saneamento", image:"img/agua.jpg"},
     {text: "ODS 7 - Energia Limpa e Acessível", image:"img/energia.jpg"},
-    {text: "ODS 11 - Cidades Sustentáveis", image:"img/cidade.jpg"},
-    {text: "ODS 12 - Consumo Responsável", image:"img/consumo.jpg"},
-    {text: "ODS 13 - Ação Contra as Mudanças Climáticas", image:"img/clima.jpg"},
-    {text: "ODS 14 - Vida na Água", image:"img/oceano.jpg"},
-    {text: "ODS 15 - Vida Terrestre", image:"img/floresta.jpg"}
-
+    {text: "ODS 16 - Paz, Justiça e Instituições Fortes", image:"img/governo.jpg"}
     ];
         
     let i = 0;
